50df05e0b26bd2a268ec105baf91470e3b8ad8cb
tecs <ttecss@gmail.com>
2022-02-14 19:44:31 +0200

    Attach to the application when initializing the library

--- src/cli/commands/init.js
+++ src/cli/commands/init.js
@@ -1,8 +1,9 @@
 const supportedAlgos = ['EdDSA'];
 
-module.exports = ({ algo, header }, { addToConfig }) => {
+module.exports = ({ algo, header }, { addToConfig, addAppToRoot }) => {
   if (!supportedAlgos.includes(algo)) {
     throw new Error(`Unsupported algorithm, ${algo}, must be one of: [${supportedAlgos.join(', ')}]`)
   }
   addToConfig('auth:config.js', { algo, header });
+  addAppToRoot('Auth');
 };
